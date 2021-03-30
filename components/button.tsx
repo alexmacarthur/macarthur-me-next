a7da42da2faeb95848c54095d15108b37e763665
Alex MacArthur <alex@macarthur.me>
2021-03-29 22:56:48 -0500

    Upgrade Tailwind, add external icon for guest posts.

--- components/button.tsx
+++ components/button.tsx
@@ -2,6 +2,14 @@ import Link from "next/link";
 import { forwardRef } from "react";
 import Arrow from "./arrow";
 
+const ExternalIcon = () => {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" className="w-5 h-5 ml-2">
+      <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14" />
+    </svg>
+  )
+}
+
 const Button = ({
   children,
   href = "",
@@ -38,13 +46,17 @@ const Button = ({
         href={href}
       >
         {children}
-        <Arrow
+
+        {!internal && <ExternalIcon />}
+
+        {internal && <Arrow
           className={`block ${iconMargin} ${iconDimensions} ${iconRotation}`}
-        />
+        />}
       </a>
     );
   });
 
+  // Wrap in special, magic link component.
   if (internal) {
     return (
       <Link href={href}>
--- postcss.config.js
+++ postcss.config.js
@@ -1,3 +1,8 @@
 module.exports = {
-  plugins: ['postcss-import', 'postcss-nested', 'tailwindcss', 'postcss-preset-env'],
+  plugins: {
+    'postcss-import': {},
+    'postcss-nested': {},
+    '@tailwindcss/jit': {},
+    'postcss-preset-env': {}
+  },
 }
